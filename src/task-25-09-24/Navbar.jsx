c69606f0ed8c0c7d2db4fe1085b519edd6b781e6
Sharonshamitha16 <91407268+Sharonshamitha16@users.noreply.github.com>
2024-10-13 18:23:34 +0530

    Update Navbar.jsx

--- src/task-25-09-24/Navbar.jsx
+++ src/task-25-09-24/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
       <div className="container ">
         <ul className="bg-grey text-white flex justify-evenly ">
 
-        <Link to={"/nav"}><img src={Myimage} alt="" height={20} width={200} className="mb-1" />
+        <Link to={"/"}><img src={Myimage} alt="" height={20} width={200} className="mb-1" />
         </Link>
           <li className="pt-7  text-2l underline-expand text-white-10 font-semibold active:text-yellow-600 ">
-            <Link className="px-4 py-2 mx-2 rounded text-white" to={"/nav"}> Home
+            <Link className="px-4 py-2 mx-2 rounded text-white" to={"/"}> Home
             </Link></li>
           <li className="pt-7  text-2l underline-expand text-white-10 font-semibold active:text-yellow-600 ">
             <Link className="px-4 py-2 mx-2  rounded text-white" to={"/nav/About"}>
